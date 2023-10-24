588ee6e5d54afde3d512d9bfba5cd22dd600002b
Anthony Lara <Capone0824@gmail.com>
2023-10-24 19:48:51 -0400

    added stripe API and routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+var stripe = require("stripe")(process.env.STRIPE);
+
 const Cart = require("../models/Cart");
 const Product = require("../models/Product");
 
 const isAuthenticated = require("../middleware/isAuthenticated");
 
-// POST /cart/add-item/:userId - Add an item to the user's cart
+//Add an item to the user's cart
 router.post("/add-item/:userId", isAuthenticated, (req, res) => {
   const { userId } = req.params;
   const { itemId, quantity, price, name, image, size, total } = req.body;
@@ -32,7 +34,7 @@ router.post("/add-item/:userId", isAuthenticated, (req, res) => {
           quantity,
           price,
           size,
-          total
+          total,
         });
         cart.total = cart.items.reduce((acc, curr) => {
           return acc + curr.quantity * curr.price;
@@ -42,7 +44,7 @@ router.post("/add-item/:userId", isAuthenticated, (req, res) => {
       return cart.save();
     })
     .then((updatedCart) => {
-      console.log('Updated Cart ==>', updatedCart);
+      console.log("Updated Cart ==>", updatedCart);
       res.status(200).json(updatedCart);
     })
     .catch((err) => {
@@ -51,6 +53,7 @@ router.post("/add-item/:userId", isAuthenticated, (req, res) => {
     });
 });
 
+// View the users cart
 router.get("/", isAuthenticated, (req, res, next) => {
   const owner = req.user._id;
 
@@ -66,54 +69,127 @@ router.get("/", isAuthenticated, (req, res, next) => {
     });
 });
 
-router.post('/update-quantity/:thisItemId/:cartId', isAuthenticated, async (req, res, next) => {
-  const { thisItemId, cartId } = req.params;
+//Update the users cart/quantity/total
+router.post(
+  "/update-quantity/:thisItemId/:cartId",
+  isAuthenticated,
+  async (req, res, next) => {
+    const { thisItemId, cartId } = req.params;
 
-  const { quantity } = req.body;
+    const { quantity } = req.body;
 
-  try {
+    try {
+      let thisCart = await Cart.findById(cartId);
+      let thisItem = thisCart.items.find(
+        (item) => item.itemId.toString() === thisItemId
+      );
+      thisItem.quantity = quantity;
+      thisCart.total = thisCart.items.reduce((acc, curr) => {
+        return acc + curr.quantity * curr.price;
+      }, 0);
 
-    let thisCart = await Cart.findById(cartId)
-    let thisItem = thisCart.items.find((item) => item.itemId.toString() === thisItemId)
-    thisItem.quantity = quantity
-    thisCart.total = thisCart.items.reduce((acc, curr) => {
-      return acc + curr.quantity * curr.price;
-    }, 0);
+      let updatedCart = await thisCart.save();
 
-    let updatedCart = await thisCart.save()
+      console.log("Cart after quantity", updatedCart);
 
-    console.log("Cart after quantity", updatedCart)
-
-    res.json(updatedCart)
-
-  } catch(err) {
-    console.log(err)
-    res.json(err)
+      res.json(updatedCart);
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
   }
-})
+);
 
-router.get('/remove-item/:thisItemId/:cartId', isAuthenticated, async (req, res, next) => {
+//Remove item from cart
+router.get(
+  "/remove-item/:thisItemId/:cartId",
+  isAuthenticated,
+  async (req, res, next) => {
+    const { thisItemId, cartId } = req.params;
 
-  const { thisItemId, cartId } = req.params;
+    try {
+      let thisCart = await Cart.findById(cartId);
+      thisCart.items = thisCart.items.filter(
+        (item) => item.itemId.toString() !== thisItemId
+      );
+      thisCart.total = thisCart.items.reduce((acc, curr) => {
+        return acc + curr.quantity * curr.price;
+      }, 0);
 
-  try {
-    let thisCart = await Cart.findById(cartId)
-    thisCart.items = thisCart.items.filter((item) => item.itemId.toString() !== thisItemId)
-    thisCart.total = thisCart.items.reduce((acc, curr) => {
-      return acc + curr.quantity * curr.price;
-    }, 0);
+      let updatedCart = await thisCart.save();
+      console.log("Cart after delete", updatedCart);
 
-    let updatedCart = await thisCart.save()
-    console.log("Cart after delete", updatedCart)
-
-    res.json(updatedCart)
-  } catch(err) {
-    console.log(err)
-    res.json(err)
+      res.json(updatedCart);
+    } catch (err) {
+      console.log(err);
+      res.json(err);
+    }
   }
+);
 
+//Stripe API checkout
+router.post("/checkout", isAuthenticated, (req, res, next) => {
+  // const items = req.body.items;
 
-})
+  // if (!Array.isArray(items) || items.length === 0) {
+  //   return res.status(400).json({ error: "Invalid or empty items array" });
+  // }
 
+  const calcTotal = Number((req.body.total * 100).toFixed(2));
 
+  const lineItems = [
+    {
+      price_data: {
+        currency: "usd",
+        unit_amount: calcTotal,
+        product_data: {
+          name: "sole city purchase",
+        },
+      },
+      quantity: 1,
+    },
+  ];
+
+  stripe.checkout.sessions
+    .create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.REACT_APP_URI}/success`,
+      cancel_url: `${process.env.REACT_APP_URI}/cart`,
+    })
+    .then((session) => {
+      res.json({ url: session.url });
+    })
+    .catch((error) => {
+      console.error("Error creating checkout session:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+});
+
+//Clear the user's entire cart
+router.post("/clear/:cartId", isAuthenticated, (req, res) => {
+  const { cartId } = req.params;
+
+  // Find the cart by ID and set its items to an empty array and total to 0
+  Cart.findById(cartId)
+    .then((cart) => {
+      console.log('Cart being cleared', cart);
+      if (!cart) {
+        return res.status(404).json({ message: "Cart not found" });
+      }
+
+      cart.items = [];
+      cart.total = 0;
+
+      return cart.save();
+    })
+    .then((clearedCart) => {
+      console.log("Cart cleared:", clearedCart);
+      res.status(200).json(clearedCart);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
+});
 module.exports = router;
